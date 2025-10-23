f78487f9b316ad03e93e6e11ba1a3f0ae43045bc
Toshiaki Maki <makingx@gmail.com>
2025-10-23 11:53:10 +0900

    Tweak

--- src/test/java/com/example/petstore/PetstoreApplicationTests.java
+++ src/test/java/com/example/petstore/PetstoreApplicationTests.java
@@ -1,12 +1,14 @@
 package com.example.petstore;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@Testcontainers(disabledWithoutDocker = true)
 class PetstoreApplicationTests {
 
 	@Test
